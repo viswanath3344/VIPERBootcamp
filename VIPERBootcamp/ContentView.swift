68f8a385b6949a57d7597a0024b94541e0feb3a4
viswanath Reddy <viswanath3344@gmail.com>
2024-11-20 09:57:20 +0530

    Initial Commit

---
+++ VIPERBootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VIPERBootcamp
+//
+//  Created by C8V7P2 on 20/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VIPERBootcamp/VIPERBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  VIPERBootcampApp.swift
+//  VIPERBootcamp
+//
+//  Created by C8V7P2 on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct VIPERBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
